f5bcca428495d4608a1f2a645253a7cfaed4eaa1
marcos/psouza <137806232+marcospsouz@users.noreply.github.com>
2025-06-17 11:10:24 -0300

    fds

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+
+console.log(botoes)
